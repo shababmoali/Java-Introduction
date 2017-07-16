cf834c8070c8ce7b1912a8f2cf6a068663b649b6
shababali <shabab.ali@mail.utoronto.ca>
2017-07-16 13:34:10 -0700

    completed draw() to set corner and print to Screen object

--- Assignment_6_Classes_AnimationObjects/Sprite.java
+++ Assignment_6_Classes_AnimationObjects/Sprite.java
@@ -16,17 +16,15 @@ public class Sprite {
 	Scanner f = new Scanner(inF);
 	height = f.nextInt();
 	width = f.nextInt();
+	// remember to call an extra nextLine() after nextInt() when reading the file
 	f.nextLine();
-    // remember to call an extra nextLine() after nextInt() when reading the file
     
-    // you need to use nextLine() and not next(), in order to
-    // allow you to use space chars in the sprite file
-    
-    // setup the characters in array art
-	//String s;
+    // setup the characters in array art	
 	art = new char[height][width];
     for (int i=height-1; i>=0; i--) {
-		String s = f.nextLine();
+	    // you need to use nextLine() and not next(), in order to
+		// allow you to use space chars in the sprite file - read through String s;
+    	String s = f.nextLine();
 		for (int j=0; j<s.length(); j++) {
 			art[i][j] = s.charAt(j);
 		}
@@ -36,6 +34,7 @@ public class Sprite {
 		}
       
 	}
+  
   }
   
   public void setCorner(double x, double y) {
@@ -54,16 +53,37 @@ public class Sprite {
   }
   
   public void draw() {
-    // send chars to panel
-	for (double i=p.y; i<height; i++) {
-		for (double j=p.x; j<width; j++) {
+    
+	// Establish dimensions of Screen object that will receive characters from the Sprite object.
+	// Bottom left corner ie art(x,y) = art(0,0) of the Sprite object. 
+	// The screen origin or corner position Point p is instantiated by
+	// instance method setCorner() call in method main of Program class Picture.
+	int startY = (int)p.y;
+	int stopY = (int)p.y + (height-1);
+	int startX = (int)p.x;
+	int stopX = (int)p.x + (width-1);
+	
+	// send art[][] chars to panel
+	int yChar = 0;
+	for (int i=startY; i<=stopY; i++) {
+		
+		int xChar = 0;
+		for (int j=startX; j<=stopX; j++) {
 			//System.out.print((art[i][j]));
-			panel.set( (int)j, (int)i, get((int)j,(int)i) );
-		}      
+			// don't draw space chars to panel
+			if (get(xChar,yChar) != ' ') {
+				panel.set( j, i, get(xChar,yChar) );
+			}
+			xChar++;
+		}
+		yChar++;
+		//System.out.print(" 			" + y);
+		//System.out.print(" 			" + x);
 		//System.out.println();
+
 	}
 	
     
-    // don't draw space chars to panel
+    
   }
 }
