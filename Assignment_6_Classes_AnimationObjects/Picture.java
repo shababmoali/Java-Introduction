c25d73a1b67b4844dcc77f84943b82fef36b1fa9
shababali <shabab.ali@mail.utoronto.ca>
2017-07-16 13:36:53 -0700

    completed main() to loop and print multiple Sprite objects to Screen object "window"

--- Assignment_6_Classes_AnimationObjects/Picture.java
+++ Assignment_6_Classes_AnimationObjects/Picture.java
@@ -12,21 +12,42 @@ public class Picture {
     // you don't need to call clear() to set up 
     // anymore with new version of Screen
 
-/*    
+
+    
     // set up your sprite filenames here
     String[] filenames = { 
       "alien1.txt",
-      "alien1.txt",
-      "alien1.txt" 
+      "alien2.txt",
+      "alien3.txt" 
     };
-*/
-    
-    // make a Sprite array
+
 	
-	Sprite a1 = new Sprite("alien2.txt");
-	a1.setCorner(0, 0);
+    // make a Sprite array
+	Sprite[] alien = new Sprite[3]; 
+	for (int i=0; i<3; i++) {
+		alien[i] = new Sprite(filenames[i]); 
+	}
+	
+	// print 9 Sprite objects to the Screen objects
+	for (int c=0; c<3; c++) {
+		
+	  for (int i=0; i<3; i++) {	
+		  alien[i].setCorner(r.nextInt(130), r.nextInt(30));
+		  alien[i].put(window);
+		  alien[i].draw();
+	  }  
+		
+	}
+
+	
+
+/*
+	Sprite a1 = new Sprite("alien1.txt");
+	a1.setCorner(10, 25);
 	a1.put(window);
 	a1.draw();
+
+*/	
     
     // use loops to draw each Sprite at three different Points
     
