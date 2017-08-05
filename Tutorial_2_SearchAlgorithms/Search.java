0b94fbb0722c5bc2a47377226e1a20218b83f264
shababali <shabab.ali@mail.utoronto.ca>
2017-08-05 00:10:15 -0700

    completed demo implementation of linearsearch() and binarysearch()
    git
    
    $
    "

--- Tutorial_2_SearchAlgorithms/Search.java
+++ Tutorial_2_SearchAlgorithms/Search.java
@@ -4,19 +4,24 @@ import java.util.Arrays;
 public class Search {
 
 
+	// method boolean linearSearch(int[] A, int k) iterates through the array
+	// to find int k and returns true if found. Time complexity O(n).
 	public static boolean linearSearch(int[] A, int k) {
 	
 		for (int i=0; i<A.length; i++) {
 			if (A[i] == k) {
+				System.out.println("True");
 				return true;
 			}
 		}
-		
+		System.out.println("False");
 		return false;
 	
 	} // end method boolean linearSearch(int[] A, int k)
 
 	
+	// method boolean binarySearch(int[] A, int k) partitions a sorted array
+	// to find int k and returns true if found. Time complexity O(n).
 	public static boolean binarySearch(int[] A, int k) {
 		
 		int min = 0;
@@ -25,16 +30,18 @@ public class Search {
 		while (min <= max) {
 			int mid = (min + max)/2;
 			System.out.printf("min: %d, med: %d, max: %d \n", min, mid, max);
-			if (A[mid] < k) { // eliminate left
-				min = mid + 1;
-				System.out.printf("R - min: %d, med: %d, max: %d \n", min, mid, max);
-			} else if (A[mid] > k) { //eliminate right
-				max = mid - 1;
-				System.out.printf("L - min: %d, med: %d, max: %d \n", min, mid, max);
-			// A[mid] == k
-			} else { 
+			// A[mid] == k ? => k is found, return true;
+			if (A[mid] == k) {
 				System.out.println("True");
 				return true;
+			// A[mid] > k ? => eliminate right
+			} else if (A[mid] > k) { 
+				max = mid - 1;
+				System.out.printf("L - min: %d, med: %d, max: %d \n", min, mid, max);
+			// A[mid] < k ? => eliminate left
+			} else { 				
+				min = mid + 1;
+				System.out.printf("R - min: %d, med: %d, max: %d \n", min, mid, max);
 			}
 		}
 		
@@ -47,12 +54,28 @@ public class Search {
 	
 	public static void main(String[] args) {
 		
-		int[] A = {2,3,4,5,6,7,8,9,10};
-		int num = 2;
+		int[] A = {2,3,4,5,6,7,8,9,10,10,12,13,14,15,16};
 		
-		binarySearch(A, num);
+		int numTrue = 10;
+		int numFalse = 11;
+		
+		//Linear Search True Test
+		System.out.println();
+		linearSearch(A, numTrue);
+		
+		//Binary Search True Test
+		System.out.println();
+		binarySearch(A, numTrue);
+		
+		//Linear Search False Test
+		System.out.println();
+		linearSearch(A, numFalse);
+		
+		//Binary Search True Test
+		System.out.println();
+		binarySearch(A, numFalse);
 	
 	}
 
 
-}
+} // end Program class Search
